1bcad0e3d1080cb050e7a70fab924988066b0f6b
Andres Rivero <andres19rivero@gmail.com>
2024-09-19 20:41:02 -0400

    feat(auth): implement & attach register form

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { AuthfireService } from '../../../services/authfire.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
+  private formBuilder = inject(FormBuilder);
+  private authFire = inject(AuthfireService);
+  private router = inject(Router);
 
+  regiterForm = this.formBuilder.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    ci: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+  constructor() {}
+
+  onSubmit() {
+    if (this.regiterForm.invalid) return;
+  }
 }
