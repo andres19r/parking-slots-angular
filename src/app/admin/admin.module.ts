dff4f149d2d5a8048acccfdb29b679f2bb8f8add
Andres Rivero <andres19rivero@gmail.com>
2024-09-23 19:41:03 -0400

    feat(admin): generate admin module with users-table component

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UsersTableComponent } from './pages/users-table/users-table.component';
+
+
+
+@NgModule({
+  declarations: [
+    UsersTableComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AdminModule { }
