7ac263f51194627f834af017fcb131ad594f5bc6
Andres Rivero <andres19rivero@gmail.com>
2024-09-23 19:36:57 -0400

    feat(auth): implement register form component

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,7 +1,9 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
-import { AuthfireService } from '../../../services/authfire.service';
 import { Router } from '@angular/router';
+import { FirestoreService } from '../../../services/firestore.service';
+import { User } from '../../../models/user';
+import { AuthfireService } from '../../../services/authfire.service';
 
 @Component({
   selector: 'app-register',
@@ -10,19 +12,35 @@ import { Router } from '@angular/router';
 })
 export class RegisterComponent {
   private formBuilder = inject(FormBuilder);
-  private authFire = inject(AuthfireService);
+  private firestoreService = inject(FirestoreService);
   private router = inject(Router);
+  private authFire = inject(AuthfireService);
 
   regiterForm = this.formBuilder.group({
-    firstName: ['', Validators.required],
-    lastName: ['', Validators.required],
-    ci: ['', Validators.required],
-    email: ['', Validators.required],
-    password: ['', Validators.required],
+    firstName: ['name', Validators.required],
+    lastName: ['last', Validators.required],
+    ci: [12345, Validators.required],
+    email: ['user@email.com', [Validators.required, Validators.email]],
+    password: ['123456', Validators.required],
   });
   constructor() {}
 
   onSubmit() {
     if (this.regiterForm.invalid) return;
+
+    const { firstName, lastName, ci, email, password } = this.regiterForm.value;
+    const newUser = new User(firstName!, lastName!, ci!, email!, password!);
+    this.firestoreService
+      .createUser(newUser)
+      .then((user) => {
+        console.log(user);
+        return this.authFire.register(email!, password!);
+      })
+      .then(() => {
+        this.router.navigateByUrl('/parking');
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   }
 }
