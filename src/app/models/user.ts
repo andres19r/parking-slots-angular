862daf4650db9aee0faf2338c53f01c566de2b73
Andres Rivero <andres19rivero@gmail.com>
2024-09-23 21:03:31 -0400

    refactor(models): include uid and serializer in user model

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,10 +1,31 @@
+interface UserSerializer {
+  uid: string;
+  firstName: string;
+  lastName: string;
+  ci: number;
+  email: string;
+  role: UserRole;
+}
+
 export class User {
+  static fromFirebase({
+    uid,
+    firstName,
+    lastName,
+    ci,
+    email,
+    role,
+  }: UserSerializer) {
+    return new User(uid, firstName, lastName, ci, email, role);
+  }
+
   constructor(
+    public uid: string,
     public firstName: string,
     public lastName: string,
     public ci: number,
     public email: string,
-    public role: UserRole,
+    public role: UserRole
   ) {}
 }
 
