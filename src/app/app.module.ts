0a00d4e4f0bf38ce0515fe200b0de78341c2720a
Andres Rivero <andres19rivero@gmail.com>
2024-09-24 21:00:43 -0400

    feat(parking): implement home parking page template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { environment } from '../environments/environment';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { AuthModule } from './auth/auth.module';
 import { AdminModule } from './admin/admin.module';
+import { ParkingModule } from './parking/parking.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,6 +17,7 @@ import { AdminModule } from './admin/admin.module';
     BrowserModule,
     AuthModule,
     AdminModule,
+    ParkingModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFireAuthModule,
--- src/app/parking/parking.module.ts
+++ src/app/parking/parking.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './pages/home/home.component';
-
-
+import { MaterialModule } from '../material/material.module';
 
 @NgModule({
-  declarations: [
-    HomeComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HomeComponent],
+  imports: [CommonModule, MaterialModule],
 })
-export class ParkingModule { }
+export class ParkingModule {}
