bd01ba4b644f5a06ace7336a530cc20b1e6a4178
Andres Rivero <andres19rivero@gmail.com>
2024-09-25 20:01:21 -0400

    feat(parking): implement home page to check parking slots

--- src/app/parking/pages/home/home.component.ts
+++ src/app/parking/pages/home/home.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ParkingService } from '../../../services/parking.service';
+
+interface ParkingSlot {
+  position: number;
+  available: boolean;
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  parkingSlots: ParkingSlot[] = [];
+  constructor(private parkingService: ParkingService) {}
 
+  ngOnInit(): void {
+    this.parkingService.getParkingSlots().subscribe((slots: any) => {
+      this.parkingSlots = slots;
+    });
+  }
 }
